db5bd48b5a475e8e556bf099a379e35686d260a7
Bryce Thompson <9068303+thompsonbryce@users.noreply.github.com>
2024-06-25 07:53:01 +0800

    New function app test

--- src/SampleApi.cs
+++ src/SampleApi.cs
@@ -1,13 +1,10 @@
 ﻿using System.Net;
 using System.Text.Json;
-using Azure;
 using Grpc.Core;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 
 namespace SampleApi;
@@ -34,18 +31,18 @@ public class SampleApi
     {
         var response = req.CreateResponse(HttpStatusCode.OK);
         response.Headers.Add("Content-Type", "text/json; charset=utf-8");
-        response.WriteString(JsonSerializer.Serialize(new { StatusCode.OK, Message = "Healthy"}));
+        response.WriteString(JsonSerializer.Serialize(new { StatusCode.OK, Message = "Healthy" }));
 
         return response;
-    }    
-    
+    }
+
     [OpenApiOperation(operationId: "get", tags: new[] { "get" }, Summary = "get", Description = "get")]
     [OpenApiParameter("id", Description = "The id of the item", Required = true)]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(Item), Summary = "The response", Description = "This returns the response")]
     [OpenApiResponseWithoutBody(HttpStatusCode.NotAcceptable)]
     [OpenApiResponseWithoutBody(HttpStatusCode.NotFound)]
     [OpenApiResponseWithoutBody(HttpStatusCode.TooManyRequests)]
-    
+
     [Function("SampleGet")]
     public HttpResponseData Get([HttpTrigger(AuthorizationLevel.Function, "get", Route = "sample/{id}")] HttpRequestData req, int id)
     {
@@ -66,7 +63,7 @@ public class SampleApi
     [OpenApiResponseWithoutBody(HttpStatusCode.NotAcceptable)]
     [OpenApiResponseWithoutBody(HttpStatusCode.NotFound)]
     [OpenApiResponseWithoutBody(HttpStatusCode.TooManyRequests)]
-    
+
     [Function("SamplePost")]
     public async Task<HttpResponseData> Post([HttpTrigger(AuthorizationLevel.Function, "post", Route = "sample")] HttpRequestData req)
     {
@@ -92,7 +89,7 @@ public class SampleApi
     [OpenApiResponseWithoutBody(HttpStatusCode.NotAcceptable)]
     [OpenApiResponseWithoutBody(HttpStatusCode.NotFound)]
     [OpenApiResponseWithoutBody(HttpStatusCode.TooManyRequests)]
-    
+
     [Function("SamplePatch")]
     public async Task<HttpResponseData> Patch([HttpTrigger(AuthorizationLevel.Function, "patch", Route = "sample/{id}")] HttpRequestData req, int id)
     {
@@ -119,7 +116,7 @@ public class SampleApi
     [OpenApiResponseWithoutBody(HttpStatusCode.NotAcceptable)]
     [OpenApiResponseWithoutBody(HttpStatusCode.NotFound)]
     [OpenApiResponseWithoutBody(HttpStatusCode.TooManyRequests)]
-    
+
     [Function("SampleDelete")]
     public HttpResponseData Delete([HttpTrigger(AuthorizationLevel.Function, "delete", Route = "sample/{id}")] HttpRequestData req, int id)
     {
@@ -132,6 +129,20 @@ public class SampleApi
 
         return response;
     }
+
+
+    // [Function("SampleDelete")]
+    // public HttpResponseData GetData([HttpTrigger(AuthorizationLevel.Anonymous, "getdata", Route = "sample/{id}")] HttpRequestData req, int id)
+    // {
+    //     ValidateId(id);
+    //     var item = _service.Get(id);
+
+    //     var response = req.CreateResponse(HttpStatusCode.OK);
+    //     response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+    //     response.WriteString(JsonSerializer.Serialize(item));
+
+    //     return response;
+    // }
 
     private void ValidateId(int id)
     {
